27edfad5e97d82fd329c29e25c5f2f519ba616e2
Danny Padilla <dpadilla010@gmail.com>
2021-12-12 14:44:47 -0800

    ADD - table template

--- src/components/AddGroup.js
+++ src/components/AddGroup.js
@@ -7,8 +7,31 @@ import axios from "axios";
 function AddGroup() {
     return (
         <>
-            <h2>Add Group</h2>
+
+        <h2>Add Group</h2>
+
+        <div>
+            <table border="1" cellpadding="5" cellspacing="2">
+                <thead>
+                    <tr>
+                        <th>Group</th>
+                        <th>Members</th>
+                    </tr>
+                </thead>
+                
+                <tbody>
+                    <tr>
+                        <td>Minnows</td>
+                        <td>John, Luke</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div>
             <Link to="/groups">Back</Link>
+        </div>
+
         </>
     );
 }
