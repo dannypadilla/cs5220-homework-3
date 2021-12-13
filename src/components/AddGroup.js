bc44b0ed2534b6a70e51f3ac7a31e15eecd0fed2
Danny Padilla <dpadilla010@gmail.com>
2021-12-12 20:12:34 -0800

    ADD - use post to submit

--- src/components/AddGroup.js
+++ src/components/AddGroup.js
@@ -5,31 +5,54 @@ import axios from "axios";
 
 
 function AddGroup() {
+    const [name, setGroup] = useState("");
+
+    let history = useHistory();
+
+    const onSubmit = function (e) {
+        e.preventDefault();
+
+        axios.post("http://localhost:8080/groups", {
+            name
+        })
+        .then( () => history.push("/groups") );
+    };
+
     return (
         <>
 
         <h2>Add Group</h2>
 
         <div>
-            <table border="1" cellpadding="5" cellspacing="2">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>
-                            <input type="text"></input>
-                        </th>
-                    </tr>
-                </thead>
-                
-                <tbody>
-                    <tr>
-                        <td colspan="2" scope="row">
-                            <button>Add</button>
-                        </td>
-                    </tr>
-                </tbody>
 
-            </table>
+            <form onSubmit={onSubmit}>
+                <table border="1" cellPadding="5" cellSpacing="2">
+
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <td>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    value={name}
+                                    onChange={(e) => setGroup(e.target.value)}
+                                />
+                            </td>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td colSpan="2">
+                                <button>Add</button>
+                            </td>
+                        </tr>
+                    </tbody>
+
+                </table>
+            </form>
+
         </div>
 
         <div>
