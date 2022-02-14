e42876c9e0d7a05e98361c6155e8202fb0e966ba
Danny Padilla <dpadilla010@gmail.com>
2022-02-13 22:27:56 -0800

    MOD - convert to materialui components

--- src/components/LessonManagement.js
+++ src/components/LessonManagement.js
@@ -4,25 +4,41 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+
+import Typography from "@mui/material/Typography";
+
+import SchoolIcon from '@mui/icons-material/School';
+import GroupIcon from '@mui/icons-material/Group';
+
 function LessonManagement() {
     return (
         <>
-        <Container maxWidth="lg" sx={{ mt:3, mb: 4 }}>
-            <Grid container spacing={3}>
+        <Container component="main" maxWidth="sm" sx={{ mt:3, mb: 4 }}>
+            <Grid
+                container
+                justifyContent="space-between"
+            >
+                <Paper
+                    sx={{
+                        p: 2,
+                        display: 'flex',
+                        flexDirection: 'column',
+                    }}
+                >
+                    <Typography component="h1" variant="h4" align="center">
+                        Lesson Management
+                    </Typography>
 
-                <Grid item xs={12} md={4} lg={3}>
-                    <Paper
-                        sx={{
-                            p: 2,
-                            display: 'flex',
-                            flexDirection: 'column',
-                        }}
-                    >
-                    <h2>Lesson Management</h2>
-                    <Link href="/students" sx={{ mt: 3 }} >Students</Link>
-                    <Link href="/groups" sx={{ mt: 3 }}>Groups</Link>
-                    </Paper>
-                </Grid>
+                    <ButtonGroup fullWidth="true" variant="contained" aria-label="outlined primary button group">
+                        <Button href="/students" variant="contained" startIcon={<SchoolIcon />}>Students</Button>
+                        <Button href="/groups" variant="contained" startIcon={<GroupIcon />}>Groups</Button>
+                    </ButtonGroup>
+
+
+                </Paper>
+
             </Grid>
         </Container>
         </>
