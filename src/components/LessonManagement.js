366a7315edbf3b3d51413236b7d24583a40def8c
Danny Padilla <dpadilla010@gmail.com>
2022-02-13 17:39:45 -0800

    MOD - convert to materialui component

--- src/components/LessonManagement.js
+++ src/components/LessonManagement.js
@@ -1,11 +1,30 @@
-import { Link } from "react-router-dom";
+import Link from "@mui/material/Link";
 
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 
 function LessonManagement() {
     return (
         <>
-            <h2>Lesson Management</h2>
-            <Link to="/students">Students</Link> | <Link to="/groups">Groups</Link>
+        <Container maxWidth="lg" sx={{ mt:3, mb: 4 }}>
+            <Grid container spacing={3}>
+
+                <Grid item xs={12} md={4} lg={3}>
+                    <Paper
+                        sx={{
+                            p: 2,
+                            display: 'flex',
+                            flexDirection: 'column',
+                        }}
+                    >
+                    <h2>Lesson Management</h2>
+                    <Link href="/students" sx={{ mt: 3 }} >Students</Link>
+                    <Link href="/groups" sx={{ mt: 3 }}>Groups</Link>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Container>
         </>
     );
 }
